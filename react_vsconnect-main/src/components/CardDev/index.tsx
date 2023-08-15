1dfdcd601938866149ba297e71a190f0f6f12efa
Matheus Henrique <empregos97@gmail.com>
2023-08-14 23:54:45 -0300

    add: desenvolvendo um comportamento mais restrito ao estado pela interface Value

--- react_vsconnect-main/src/components/CardDev/index.tsx
+++ react_vsconnect-main/src/components/CardDev/index.tsx
@@ -1,21 +1,31 @@
-import "./style.css"
+import "./style.css";
+
+interface Techs<s> {
+  markup: s;
+  stilify: s;
+}
+
+const techs = {
+  markup: "HTML",
+  stilysh: "CSS",
+  framework: "react",
+};
 
 export default function CardDev(props: any) {
-
-    return (
-        <div className="dev">
-            <div className="grupo_contato">
-                <img src="https://github.com/Thiago-Nascimento.png" alt="" />
-                <div className="contato_dev">
-                    <h3>{ props.nome }</h3>
-                    <p>{ props.email }</p>
-                </div>
-            </div>
-            <div className="techs">
-                <span>HTML</span>
-                <span>CSS</span>
-                <span>React</span>
-            </div>
+  return (
+    <div className="dev">
+      <div className="grupo_contato">
+        <img src={props.foto} alt="" />
+        <div className="contato_dev">
+          <h3>{props.nome}</h3>
+          <p>{props.email}</p>
         </div>
-    )
+      </div>
+      <div className="techs">
+        <span>HTML</span>
+        <span>CSS</span>
+        <span>React</span>
+      </div>
+    </div>
+  );
 }
